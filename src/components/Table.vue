3e4bc4bd0b6d86af7296f04dc17e6394bb8193fd
phoavn96 <12345Bien>
2021-06-17 20:01:54 +0700

    ok

---
+++ src/components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+
+
+    <table>
+        <thead>
+        <tr>
+            <th v-for="column in columns">{{column.label}}</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in data">
+            <td v-for="column in columns">
+
+                <template v-if="column.field !== 'action'">
+                    {{ item[column.field] }}
+                </template>
+                <template v-else>
+                    <a href="javascript:void(0)">Edit</a> | <a href="javascript:void(0)">Delete</a>
+                </template>
+            </td>
+
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "Table",
+        props: {
+            data: {
+                type: Array,
+                default: () => []
+            },
+            columns: {
+                type: Array,
+                default: () => []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Student.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <div>
+        <Table :columns="columns" :data="students"></Table>
+    </div>
+</template>
+
+<script>
+    import Table from "../components/Table";
+
+    export default {
+        name: "Student",
+        components: {
+            Table
+        },
+
+        data() {
+            return {
+                title: 'student management',
+                students: [],
+                columns: [
+                    {
+                        label: 'Roll Number',
+                        field: 'rollNumber',
+                    },
+                    {
+                        label: 'Full Name',
+                        field: 'fullName',
+                    },
+                    {
+                        label: 'Email',
+                        field: 'email',
+                    }, {
+                        label: 'Status',
+                        field: 'status',
+                    },
+                    {
+                        label: 'Action',
+                        field: 'action',
+                    }]
+
+
+            }
+        },
+        mounted() {
+            this.loadStudent()
+        },
+
+        methods: {
+            loadStudent() {
+                fetch('http://localhost:8888/student/list')
+                    .then(response => response.json())
+                    .then(data => this.students = data)
+            }
+        }
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
